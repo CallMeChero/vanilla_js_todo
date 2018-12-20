c5b82558006f6a9fa8d3129839313bb59ee93be6
CallMeChero <david.ceranic@gmail.com>
2018-12-20 10:42:22 +0100

    Final Commit

---
+++ todo.js
@@ -0,0 +1,49 @@
+var userInput = document.getElementById('userInput');
+var enterButton = document.getElementById('enter');
+var ul = document.querySelector('ul');
+var listItem = document.getElementsByTagName('li');
+
+function inputLength() {
+    return userInput.value.length;
+}
+function listLength() {
+    return listItem.length;
+}
+
+function createListElement() {
+    var li = document.createElement('li');
+    var liText = document.createTextNode(userInput.value);
+    li.appendChild(liText);
+    ul.appendChild(li);
+    userInput.value = '';
+
+    li.addEventListener("click", addNewStyle);
+    function addNewStyle() {
+        li.classList.toggle("done");
+    }
+
+    var deleteButton = document.createElement("button");
+    deleteButtonX = document.createTextNode("X");
+    deleteButton.appendChild(deleteButtonX);
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener("click", deleteItem)
+    
+    function deleteItem() {
+        li.classList.toggle("delete");
+    }
+}
+
+function AddListAfterClick() {
+    if(inputLength() > 0) {
+        createListElement();
+    }
+}
+
+function AddListAfterEnter(event) {
+    if(inputLength() > 0 && event.which === 13) {
+        createListElement();
+    }
+}
+
+enterButton.addEventListener("click", AddListAfterClick);
+userInput.addEventListener("keypress", AddListAfterEnter)
